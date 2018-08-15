fc85bb7afd5432b231757e35e1c8996e219a615b
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-15 00:06:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function destructivelyAppendDriver(name) {
 function destructivelyPrependDriver(name) {
   drivers.unshift(name);
 }
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop()
+}
