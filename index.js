c0f4082a5f1253e129d4f24d9cd9576fc40acde1
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-15 00:18:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,19 +3,15 @@ const drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(name) {
   drivers.push(name);
 }
-
 function destructivelyPrependDriver(name) {
   drivers.unshift(name);
 }
-
 function destructivelyRemoveFirstDriver() {
   drivers.shift();
 }
-
 function destructivelyRemoveLastDriver() {
   drivers.pop()
 }
-
 function appendDriver(name) {
   let newArray = [...drivers, name];
 }
