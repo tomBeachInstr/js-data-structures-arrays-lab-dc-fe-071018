4bd6d674c16f4d9bb375de961f673ed0b807780e
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-15 00:50:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+function destructivelyRemoveLastDriver() {
+  drivers.pop()
+}
+function appendDriver(name) {
+  return [...drivers, name]
+}
+function prependDriver(name) {
+  return [name, ...drivers]
+}
+function removeLastDriver() {
+  return drivers.slice(0, 2);
+}
+function removeFirstDriver() {
+  return drivers.slice(1, 3);
+}
